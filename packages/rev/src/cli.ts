97db5caab05c56dd2cc2c677fac2ed6d7d0a52ad
Pedro Cattori <pcattori@gmail.com>
2023-06-29 17:47:04 -0400

    feat(rev): basic cli skeleton

--- packages/rev/src/cli.ts
+++ packages/rev/src/cli.ts
@@ -1,2 +1,18 @@
-let a: string = 'Hello World'
-console.log(a)
+import { Command } from 'commander'
+
+let program = new Command()
+program.name('rev')
+
+program.command('build').action(() => {
+  console.log('build!')
+})
+
+program.command('serve').action(() => {
+  console.log('serve!')
+})
+
+program.command('dev').action(() => {
+  console.log('dev!')
+})
+
+program.parse()
