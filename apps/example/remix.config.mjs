67c74042f3b00626b1ca418ba494fba6dbca8519
Pedro Cattori <pcattori@gmail.com>
2023-07-06 12:15:40 -0400

    chore: silence v2 warnings

--- apps/example/remix.config.mjs
+++ apps/example/remix.config.mjs
@@ -1,3 +1,11 @@
 export default {
-  future: { v2_meta: true, v2_routeConvention: true },
+  serverModuleFormat: 'esm',
+  future: {
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
 }
