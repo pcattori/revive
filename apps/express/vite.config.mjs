6f06c872e1081928801510abb1d09d5513e05438
Pedro Cattori <pcattori@gmail.com>
2023-10-02 13:48:05 -0400

    do not prebundle @remix-run/react
    so that we can patch in the `experimental-revive` branch
    via `LOCAL_BUILD_DIRECTORY`

--- apps/express/vite.config.mjs
+++ apps/express/vite.config.mjs
@@ -5,6 +5,10 @@ import remarkRemixMdxFrontmatter from 'remark-remix-mdx-frontmatter'
 import { revive } from 'revive'
 
 export default defineConfig({
+  // do not pre-bundle @remix-run/react so we can patch it in via `LOCAL_BUILD_DIRECTORY` from the remix-run/remix repo
+  optimizeDeps: {
+    exclude: ['@remix-run/react'],
+  },
   plugins: [
     revive(),
     mdx({
